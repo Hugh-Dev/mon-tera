cf38db57b416eea248b737e8a36d37b33582573f
Hugh-Dev <hughpythoneer@gmail.com>
2022-03-09 12:50:15 -0800

    update

--- settings.py
+++ settings.py
@@ -3,21 +3,48 @@ Flask settings for config project.
 """
 import mysql.connector
 from decouple import config
+from mysql.connector import Error
 
 # SECURITY: secret host used in production!
 PORT_FLASK = config('PORT_FLASK')
 DEBUG = config('DEBUG')
 
-config = {
+"""config = {
     
     'host':config('HOST'),
     'port':config('PORT'),
     'user':config('USER'),
     'password':config('PASSWORD'),
     'database':config('DATABASE'),
-    'raise_on_warnings': True
+    'raise_on_warnings': True,
+    'connection_timeout': 1200,
 }
 
 cnx = mysql.connector.connect(**config)
 
+"""
 
+try:
+
+    config = {
+    
+    'host':config('HOST'),
+    'port':config('PORT'),
+    'user':config('USER'),
+    'password':config('PASSWORD'),
+    'database':config('DATABASE'),
+    'raise_on_warnings': True,
+    'connection_timeout': 1200,
+}
+
+    cnx = mysql.connector.connect(**config)
+
+    if cnx.is_connected():
+        db_info = cnx.get_server_info()
+
+except Error as e:
+    error = e
+
+finally:
+    if cnx.is_connected():
+        cnx.close()
